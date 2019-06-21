dd65f607e85bfcb53163709a7efc97e307d0f923
Igor Gabrig <igor_gabrig@hotmail.com>
2019-06-21 16:25:32 -0300

    Updates

---
+++ precache-manifest.a7236987551714cba846c7b6ab5505c4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4408c58145d6f234d5f79dbfdf9114e",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "bc27360e8315390b80f6",
+    "url": "/portfolio_react/static/css/main.88a6ddc8.chunk.css"
+  },
+  {
+    "revision": "02972855df72cf86af0e",
+    "url": "/portfolio_react/static/js/2.e051d0f8.chunk.js"
+  },
+  {
+    "revision": "bc27360e8315390b80f6",
+    "url": "/portfolio_react/static/js/main.9c0236cf.chunk.js"
+  },
+  {
+    "revision": "c91455f75e20bb8f5bf7",
+    "url": "/portfolio_react/static/js/runtime~main.269d08db.js"
+  },
+  {
+    "revision": "cb241707fc22412c04c441a13e655b26",
+    "url": "/portfolio_react/static/media/artificial_intelligence.cb241707.png"
+  },
+  {
+    "revision": "f54e0fb306ee0953ade041d8ae4bc611",
+    "url": "/portfolio_react/static/media/personal.f54e0fb3.jpg"
+  },
+  {
+    "revision": "f1c8904867901fb30e4f0395e8a2c85e",
+    "url": "/portfolio_react/static/media/react.f1c89048.png"
+  },
+  {
+    "revision": "75afe44fa6719a861c634cf53b943d95",
+    "url": "/portfolio_react/static/media/webservice.75afe44f.jpg"
+  },
+  {
+    "revision": "d5417a2cfce34b7b6215a20cfc6daec6",
+    "url": "/portfolio_react/static/media/xadrez.d5417a2c.jpg"
+  }
+]);
